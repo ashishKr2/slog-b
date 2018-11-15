fd9b3669fa8adf7b04843314f41fc8539e0e5ba6
Ashish Kumar <ashish.23213@lpu.co.in>
2018-11-15 17:26:10 +0530

    updated socket and pagination

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 require('dotenv').config();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const express = require('express'),
     bodyParser = require('body-parser'),
     app = express();
-const http=require('http').Server(app);
-const io=require('socket.io')(http);
+const chatSchema = require('./schema/chat');
+const http = require('http');
+const server = http.createServer(app);
+const socketIO = require('socket.io');
+
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const cors = require('cors');
 const passport = require('passport');
 const path = require('path');
+// var users=new Users();
 require('./config/passport')(passport);
 require('./config/mongo')(mongoose);
 app.use(bodyParser.json());
@@ -23,16 +27,46 @@ require("./routes")(app, passport);
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
-io.on('connection', function(socket){
+const io = socketIO(server);
+io.sockets.on('connection', function (socket) {
     console.log('a user connected');
-    socket.on('disconnect',function(){
+    socket.on('disconnect', function () {
         console.log("user disconnected");
     });
-    socket.on('message',(message)=>{
-        console.log("Message Received:"+message);
-        io.emit('message',{type:'new-message',text:message});
+    socket.on('newMessage', function (data) {
+        socket.join(data.roomName);
+
+        var newMessage = new chatSchema({
+            message: data.message,
+            sender: data.active_username_sender,
+            reciver: data.reciver
+        });
+        message1 = newMessage.save();//saving all 
+        //finding and getting messages
+        if(message1){
+            if (chatSchema.find({
+                $or: [
+                    { 'sender': data.active_username_sender },
+                    { 'sender': data.reciver }
+                ]
+            })) {
+                chatSchema.find({
+                    $or: [
+                        { 'reciver': data.reciver },
+                        { 'reciver': data.active_username_sender }
+                    ]
+                }, function (err, docs) {
+                    console.log("sdsd",data)
+                    if (!err) io.to(data.roomName).emit('newMessage', docs);
+                }
+                )
+            }
+        }
+     
+
+        // io.to(data.roomName).emit('newMessage', data);
     })
-  });
-http.listen(port, function () {
+});
+server.listen(port, function () {
     console.log('server is running on port no ' + port);
 });
--- controller/browseJob.js
+++ controller/browseJob.js
@@ -2,9 +2,17 @@ const Jobs=require('../schema/postProject');
 
 module.exports={
     browseJob: async(req,res)=>{
+        skip=req.body.skip,
+        limit=req.body.limit
         try{
-            const jobs= await Jobs.find();
-            res.json(jobs);
+            const allData =await Jobs.find();
+            const length =allData.length;
+            const jobs= await Jobs.find().skip(skip).limit(limit);
+            const data={
+                length:length,
+                jobs:jobs
+            }
+            res.json(data);
         }catch(err){
             res.status(500).json({success:false,message:'Error occured'});
         }
@@ -23,23 +31,31 @@ module.exports={
     },
     search:async(req,res)=>{
         keyword=req.body.keyword
-        const myPattern = new RegExp('(\\w* '+keyword+' \\w*)','gi');
-        const matchedKey=await Jobs.find({ 
-            // projectName:keyword
-            $or:[
-                {'projectName':myPattern},
-                {'projectDetail':myPattern},
-                {'projectSize':myPattern},
-                {'skills':myPattern}
-            ]
-        }) ;
-        if(matchedKey!=0){ //if match of keyword found >[] means not null then to return matched keyword project else whole project
-            res.json(matchedKey);
-        }
-        else{
+        if(keyword==""){
             const jobs= await Jobs.find();
             res.json(jobs);
         }
+        else{
+            const myPattern = new RegExp('(\\w* '+keyword+' \\w*)','gi');
+            const matchedKey=await Jobs.find({ 
+                // projectName:keyword
+                $or:[
+                    {'projectName':myPattern},
+                    {'projectDetail':myPattern},
+                    {'projectSize':myPattern},
+                    {'skills':myPattern}
+                ]
+            }) ;
+            if(matchedKey!=0){ //if match of keyword found >[] means not null then to return matched keyword project else whole project
+                res.json(matchedKey);
+            }
+            
+            else{
+                const jobs= await Jobs.find();
+                res.json(jobs);
+            }
+        }
+       
        
     }
 }
